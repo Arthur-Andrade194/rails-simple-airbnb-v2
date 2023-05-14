cdfa521892802a6e9fd2e2f89f60ed124beed0b0
Arthur Andrade <arthurandrade194@gmail.com>
2023-05-14 14:32:18 +0900

    controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts 'Cleaning the db....'
+Flat.destroy_all
+
+30.times do
+  Flat.create!(
+    name: Faker::Adjective.positive + ' ' + Faker::Space.star_cluster,
+    address: Faker::Address.street_name,
+    description: Faker::Lorem.words(number: 10),
+    price_per_night: Faker::Number.within(range: 1..300),
+    number_of_guests: Faker::Number.within(range: 1..5)
+  )
+end
+
+puts "... created #{Flat.count} flats"
+puts Flat.last
